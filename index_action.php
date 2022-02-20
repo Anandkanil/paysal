f473dc234f382c9ad6832994b3cd5582be76aa19
Anandkanil <anandkanil2022a@ajce.mca.in>
2022-02-20 22:37:24 +0530

    after adding login template

---
+++ index_action.php
@@ -0,0 +1,29 @@
+<?php
+var_dump($_POST);
+$conn= new mysqli('localhost','root','','payroll_db')or die("Could not connect to mysql".mysqli_error($con));
+$username=$_POST['username'];
+$password=$_POST['password'];
+$query="SELECT * from `users` where username='$username' and password='$password'";
+$exe=mysqli_query($conn,$query);
+if(mysqli_num_rows($exe))
+{
+    echo"Login SUCESSFULL!!!!";
+    $row=mysqli_fetch_array($exe);
+    $type=$row['type'];
+    echo $type;
+    if($type==0)
+    {
+        header('Location:employee/index.php');
+    }
+    else if($type==1)
+    {
+        header('Location:admin/index.php');
+    }
+}
+else
+{
+    header('Location:login.php?show=wrong userid/password');
+}
+
+
+?>
